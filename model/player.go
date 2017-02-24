dbf71324b87ef0f67e4bdf3e8965570c355a724f
Devin Beauregard <Devin Beauregard>
2017-02-24 01:39:32 -0700

    Accounting for covert weapon holding

--- model/player.go
+++ model/player.go
@@ -2,18 +2,22 @@ package model
 
 //Player contains informatiom about a weapon
 type Player struct {
-	Sov          int64
-	Note         string
-	ExternalID   int
-	Name         string
-	BattleForce  int
-	Sa           string `json:"Sa,omitempty"`
-	Da           string `json:"Da,omitempty"`
-	Sp           string `json:"Sp,omitempty"`
-	Se           string `json:"Se,omitempty"`
-	Tff          string `json:"Tff,omitempty"`
-	IsHolding    string
-	IsTrained    string
-	TotalMercs   int
-	TotalCoverts int
+	Sov             int64
+	Note            string
+	ExternalID      int
+	Name            string
+	BattleForce     int
+	Sa              string `json:"Sa,omitempty"`
+	Da              string `json:"Da,omitempty"`
+	Sp              string `json:"Sp,omitempty"`
+	Se              string `json:"Se,omitempty"`
+	Tff             string `json:"Tff,omitempty"`
+	IsHolding       string
+	SpyIsHolding    string
+	SentryIsHolding string
+	IsTrained       string
+	TotalMercs      int
+	TotalCoverts    int
+	Spies           int
+	Sentries        int
 }
